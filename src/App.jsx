71652aa4221d02caede36777a8b517b116bc8970
incrediblesaizan1 <glorioussaizan1@gmail.com>
2025-02-01 00:08:43 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,6 @@ function App() {
 
   const router = createBrowserRouter([
     {
-      path: '/',
-      element: <Login />
-    },
-    {
       path: '/dashboard',
       element: <Home />
     },
