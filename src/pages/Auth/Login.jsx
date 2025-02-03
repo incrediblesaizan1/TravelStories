a4afaccb7181b7bdc45407af32219a452bd5de5a
incrediblesaizan1 <glorioussaizan1@gmail.com>
2025-02-03 15:44:22 +0530

    login completed

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,54 +1,50 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom"; // 
+import { useNavigate } from "react-router-dom"; //
 import background from "../../assets/XSe80ORCzHU0rP2rjPdXX.png";
-import PasswordInput from "../../components/input/PasswordInput"
-import {axiosInstance} from "../../utils/axiosInstance"
-import Loaderplain from "../../components/common/Loaderplain"
-
+import PasswordInput from "../../components/input/PasswordInput";
+import { axiosInstance } from "../../utils/axiosInstance";
+import Loaderplain from "../../components/common/Loaderplain";
 
 const Login = () => {
-  const navigate = useNavigate(); 
-  const [identifier, setIdentifier] = useState("")
-  const [password, setPassword] = useState("")
-  const [error, setError] = useState("")
-  const [isLoading, setIsLoading] = useState(false)
+  const navigate = useNavigate();
+  const [identifier, setIdentifier] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-  const handleLogin = async (e) =>{
-  e.preventDefault();
-  if(!identifier){
-    setError("All fields are required.");
-    return;
-  }
-  if(!password){
-    setError("Please enter the password");
-    return;
-  }
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    if (!identifier) {
+      setError("All fields are required.");
+      return;
+    }
+    if (!password) {
+      setError("Please enter the password");
+      return;
+    }
 
-  setError("")
-  setIsLoading(true);
+    setError("");
+    setIsLoading(true);
 
-  try{
-    const response = await axiosInstance.post("/login", {
-      identifier,
-      password
-    });
-    setIsLoading(false)
-    console.log("Login successful", response.data)
-    navigate("/dashboard")
-  }catch(err){
-    setIsLoading(false)
-    console.error("Login error:", err.response?.data || err.message);
-    setError(err.response?.data?.message || "Something went wrong");
-  }
-  }
+    try {
+      const response = await axiosInstance.post("/login", {
+        identifier,
+        password,
+      });
+      setIsLoading(false);
+      console.log("Login successful", response.data);
+      navigate("/dashboard");
+    } catch (err) {
+      setIsLoading(false);
+      console.error("Login error:", err.response?.data || err.message);
+      setError(err.response?.data?.message || "Something went wrong");
+    }
+  };
 
   return (
     <div className="h-screen bg-cyan-50 overflow-hidden relative">
-
       <div className="login-ui-box right-10 -top-40 " />
       <div className="login-ui-box bg-cyan-200 -bottom-40 right-1/2" />
-
-    
 
       <div className="container h-screen flex items-center justify-center px-20 mx-auto">
         <div
@@ -60,7 +56,8 @@ const Login = () => {
               Capture Your <br /> Journey
             </h4>
             <p className="text-[15px] text-white leading-6 pr-7 mt-4">
-              Record your travel experience and memories in your personal travel journey.
+              Record your travel experience and memories in your personal travel
+              journey.
             </p>
           </div>
         </div>
@@ -69,20 +66,22 @@ const Login = () => {
           <form onSubmit={handleLogin}>
             <h4 className="text-2xl font-semibold mb-7">Login</h4>
 
-            <input 
-            type="text" 
-            placeholder="Enter your E-Mail or Username"
-           className="input-box"
-           value={identifier}
-           onChange={(e)=>setIdentifier(e.target.value)}
-           />
-           <PasswordInput
-            value={password}
-            onChange={(e)=>setPassword(e.target.value)}
+            <input
+              type="text"
+              placeholder="Enter your E-Mail or Username"
+              className="input-box"
+              value={identifier}
+              onChange={(e) => setIdentifier(e.target.value)}
             />
-            {error? <p className="text-red-500 text-xs pb-1">{error}</p>: ""}
+            <PasswordInput
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {error ? <p className="text-red-500 text-xs pb-1">{error}</p> : ""}
 
-            <button type="submit" className="btn-primary">LOGIN</button>
+            <button type="submit" className="btn-primary">
+              LOGIN
+            </button>
             {isLoading && <Loaderplain />}
             <p className="text-xs text-slate-500 text-center my-4">Or</p>
             <button
