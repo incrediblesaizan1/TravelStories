0c734b845b0ebde4b0e6b5d0720f3b570c8a6bee
incrediblesaizan1 <glorioussaizan1@gmail.com>
2025-02-04 20:01:18 +0530

    changes

--- src/components/common/ProfileInfo.jsx
+++ src/components/common/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { getInitials } from "../../utils/helper"
 import { useNavigate } from "react-router-dom"; 
+import {axiosInstance} from "../../utils/axiosInstance"
 
 const ProfileInfo = ({ userInfo }) => {
   const navigate = useNavigate()
@@ -11,7 +12,11 @@ const ProfileInfo = ({ userInfo }) => {
         {getInitials(userInfo ? userInfo.fullname : "")}
       </div>
       <div>
-        <p className='text-sm font-medium'>{userInfo.fullname || ""}</p>
+        <p className='text-lg relative left-2 top-1 font-medium'>{userInfo.fullname || ""}</p>
+        <button className='text-sm cursor-pointer hover:text-red-500 text-slate-700 underline' onClick={async()=>{
+        await axiosInstance.get("/logout")
+          navigate("/login")
+        }}>LogOut</button>
       </div>
     </div>
   )
