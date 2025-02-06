90636d3d6bf47eb094703594265ea32bf01868ec
incrediblesaizan1 <glorioussaizan1@gmail.com>
2025-02-06 22:40:04 +0530

    Create travel story

--- src/pages/Home/AddEditTravelStory.jsx
+++ src/pages/Home/AddEditTravelStory.jsx
@@ -1,47 +1,157 @@
-import React from 'react'
-import {MdAdd, MdClose, MdDeleteOutline, MdUpdate} from "react-icons/md"
+import React, { useState } from "react";
+import { MdAdd, MdClose, MdDeleteOutline, MdUpdate } from "react-icons/md";
+import DateSelector from "../../components/common/DateSelector";
+import { useScrollTrigger } from "@mui/material";
+import ImageSelector from "../../components/common/ImageSelector";
+import TagInput from "../../components/common/TagInput";
+import { axiosInstance } from "../../utils/axiosInstance"
+import moment from "moment";
+import { toast } from "react-toastify";
+import uploadImage from "../../utils/uploadImage"
 
-const AddEditTravelStory = ({storyInfo, type, onClose, getAllTravelStories}) => {
+const AddEditTravelStory = ({
+  storyInfo,
+  type,
+  onClose,
+  getAllTravelStories,
+}) => {
+  const [visitedDate, setVisitedDate] = useState(null);
+  const [title, setTitle] = useState("");
+  const [storyImg, setStoryImg] = useState(null);
+  const [story, setStory] = useState("");
+  const [visitedLocation, setVisitedLocation] = useState([]);
+  const [error, setError] = useState("")
 
-    const handleAddOrUpdateClick = () =>{
 
+  const updateTravelStory = () =>{}
+
+
+  const addNewTravelStory = async() =>{
+    try {
+      let imageUrl = ""
+
+      if(storyImg){
+        const imgUploadRes = await uploadImage(storyImg)
+        imageUrl = imgUploadRes.imageUrl || "";
+      }
+     
+      const response = await axiosInstance.post("/travelStory",{
+
+          "title":title,
+          "story": story,
+          "visitedLocation":visitedLocation,
+          "imageUrl": imageUrl || "",
+          "visitedDate": visitedDate ? moment(visitedDate).valueOf():moment().valueOf()
+
+      })
+
+      if(response.data && response.data.story){
+        toast.success("Story Added Successfully")
+        getAllTravelStories()
+        onClose()
+      }
+
+    } catch (error) {
+      
     }
+  }
+ 
+  const handleAddOrUpdateClick = () => {
+    console.log({title, storyImg, story, visitedDate, visitedLocation})
+
+    if(!title){
+      setError("Please enter the title")
+      return
+    }
+
+    if(!story){
+      setError("Please enter the story")
+      return
+    }
+
+    setError("")
+
+    if(type === "edit"){
+      updateTravelStory();
+    }else{
+      addNewTravelStory()
+    }
+    
+  };
+
+  const handleDeleteStoryImg = async () => {};
 
   return (
     <div>
-      <div className='flex items-center justify-between'>
-        <h5 className='text-xl font-medium text-slate-700'>
-            {type === "add"? "Add Story": "Update Story"}
+      <div className="flex items-center justify-between">
+        <h5 className="text-xl font-medium text-slate-700">
+          {type === "add" ? "Add Story" : "Update Story"}
         </h5>
         <div>
-            <div className='flex items-center gap-3 bg-cyan-50/50 p-2 rounded-l-lg'>
-               { type === "add"? <button className='btn-small' onClick={handleAddOrUpdateClick}>
+          <div className="flex items-center gap-3 bg-cyan-50/50 p-2 rounded-l-lg">
+            {type === "add" ? (
+              <button className="btn-small" onClick={handleAddOrUpdateClick}>
                 <MdAdd className="text-lg" /> ADD STORY
-                </button>:(
-                    <>
-                    <button className='btn-small' onClick={handleAddOrUpdateClick}>
-                      <MdUpdate className='text-lg' /> UPDATE STORY  
-                    </button>
-
-                
-
-                    </>
-                )}
-                <button className='cursor-pointer' onClick={onClose}>
-                <MdClose className="text-xl text-slate-400" />
+              </button>
+            ) : (
+              <>
+                <button className="btn-small" onClick={handleAddOrUpdateClick}>
+                  <MdUpdate className="text-lg" /> UPDATE STORY
                 </button>
-            </div>
+              </>
+            )}
+            <button className="cursor-pointer" onClick={onClose}>
+              <MdClose className="text-xl text-slate-400" />
+            </button>
+          </div>
+
+{error && (
+  <p className="text-red-500 text-xs pt-2 text-right">{error}</p>
+)}
+
         </div>
       </div>
 
       <div>
-        <div className=' flex-1 flex flex-col gap-2 pt-4'>
-        <label className='input-label'>TITLE</label>
-        <input type="text" className='text-2xl text-slate-950 outline-none' placeholder='A day at a great wall' />
+        <div className=" flex-1 flex flex-col gap-2 pt-4">
+          <label className="input-label">TITLE</label>
+          <input
+            type="text"
+            className="text-2xl text-slate-950 outline-none"
+            placeholder="A day at a great wall"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <div className="my-3">
+            <DateSelector date={visitedDate} setDate={setVisitedDate} />
+          </div>
+
+          <ImageSelector
+            image={storyImg}
+            setImage={setStoryImg}
+            handleDeleteImg={handleDeleteStoryImg}
+          />
+
+          <div className="flex flex-col gap-2 mt-4">
+            <label className="input-label">STORY</label>
+            <textarea
+              type="text"
+              className="text-sm resize-none text-slate-950 outline-none bg-slate-100 p-2 roudned"
+              placeholder="Your Story"
+              rows={10}
+              value={story}
+              onChange={(e) => setStory(e.target.value)}
+            ></textarea>
+          </div>
+
+          <div className="pt-3">
+            <label className="input-label">VISITED LOCATION</label>
+            <TagInput tags={visitedLocation} setTags={setVisitedLocation} />
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AddEditTravelStory
+export default AddEditTravelStory;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,133 +1,148 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import { axiosInstance } from "../../utils/axiosInstance";
-import Loader2 from "../../components/common/Loader2"
-import NoLoggedIn from "../../components/common/NoLoggedIn"
-import Navbar from '../../components/common/Navbar';
-import TravelStoryCard from "../../components/common/TravelStoryCard"
-import { ToastContainer, toast } from 'react-toastify';
-import "react-toastify/dist/ReactToastify.css"
-import { MdAdd } from 'react-icons/md';
-import Modal from "react-modal"
-import AddEditTravelStory from "./AddEditTravelStory"
+import Loader2 from "../../components/common/Loader2";
+import NoLoggedIn from "../../components/common/NoLoggedIn";
+import Navbar from "../../components/common/Navbar";
+import TravelStoryCard from "../../components/common/TravelStoryCard";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { MdAdd } from "react-icons/md";
+import Modal from "react-modal";
+import AddEditTravelStory from "./AddEditTravelStory";
 
 const Home = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(null)
-  const [userInfo, setUserInfo] = useState(null)
-  const [allStories, setAllStories] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoggedIn, setIsLoggedIn] = useState(null);
+  const [userInfo, setUserInfo] = useState(null);
+  const [allStories, setAllStories] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   const [openAddEditModal, setOpenAddEditModal] = useState({
     isShown: false,
     type: "add",
-    data: null
-  })
+    data: null,
+  });
 
-
-  const checkLoggedIN = async() =>{
+  const checkLoggedIN = async () => {
     try {
-     const user = await axiosInstance("/user")
-     setUserInfo(user.data.user)
-     await getAlltravelStories()
-     setIsLoggedIn(true)
+      const user = await axiosInstance("/user");
+      setUserInfo(user.data.user);
+      await getAlltravelStories();
+      setIsLoggedIn(true);
     } catch (error) {
-      setUserInfo(null)
-      setIsLoggedIn(false)
+      setUserInfo(null);
+      setIsLoggedIn(false);
     }
-  }
+  };
 
-  const getAlltravelStories =  async() =>{
+  const getAlltravelStories = async () => {
     try {
-      const response = await axiosInstance.get("/get-user-travelStories")
-      setAllStories(response.data.stories.reverse())
-      setIsLoading(false)
+      const response = await axiosInstance.get("/get-user-travelStories");
+      setAllStories(response.data.stories.reverse());
+      setIsLoading(false);
     } catch (error) {
-      console.log("An unexpected error occurred while fetching travel stories.")
-      setIsLoading(false)
+      console.log(
+        "An unexpected error occurred while fetching travel stories."
+      );
+      setIsLoading(false);
     }
-  }
+  };
 
-  const handleEdit = (data) =>{}
-  const handleViewStory = (data) =>{}
+  const handleEdit = (data) => {};
+  const handleViewStory = (data) => {};
 
-  const updateIsFavorite = async(storyData) =>{
-    setIsLoading(true)
-    storyData.isFavourite = !storyData.isFavourite; 
-    await axiosInstance.put(`/update-is-favourite/${storyData._id}`,{
-      "isFavourite": storyData.isFavourite
-    })
-    toast.success("Story Updated Successfully")
-    getAlltravelStories()
+  const updateIsFavorite = async (storyData) => {
+    setIsLoading(true);
+    storyData.isFavourite = !storyData.isFavourite;
+    await axiosInstance.put(`/update-is-favourite/${storyData._id}`, {
+      isFavourite: storyData.isFavourite,
+    });
+    toast.success("Story Updated Successfully");
+    getAlltravelStories();
     setTimeout(() => {
-      setIsLoading(false)
+      setIsLoading(false);
     }, 400);
-  }
+  };
 
   useEffect(() => {
-  checkLoggedIN()
-  }, [])
+    checkLoggedIN();
+  }, []);
 
-  
-  if (isLoggedIn === null) return <Loader2 />
+  if (isLoggedIn === null) return <Loader2 />;
 
   return (
     <>
-    {!isLoggedIn?<NoLoggedIn /> :(
-      <>
-      <Navbar userInfo={userInfo} />
-      <div className='container mx-16 py-10'>
-      <div className='flex gap-7'>
-{isLoading? <Loader2 />:(
-  <div className='flex-1'>
-  {allStories.length > 0 ? (
-    <div className='grid grid-cols-3 gap-4 w-[90vw]'>
-      {allStories.map((item) => {
-        return (
-          <TravelStoryCard
-            key={item._id}
-            imgUrl={item.imageUrl}
-            title={item.title}
-            story={item.story}
-            date={item.visitedDate}
-            visitedLocation={item.visitedLocation}
-            isFavourite={item.isFavourite}
-            onEdit={() => handleEdit(item)}
-            onClick={() => handleViewStory(item)}
-            onFavouriteClick={() => updateIsFavorite(item)}
-          />
-        );
-      })}
-    </div>
-  ) : (
-    <div className="text-center py-4">No travel stories found.</div>
-  )}
-</div>
-)}
+      {!isLoggedIn ? (
+        <NoLoggedIn />
+      ) : (
+        <>
+          <Navbar userInfo={userInfo} />
+          <div className="container mx-16 py-10">
+            <div className="flex gap-7">
+              {isLoading ? (
+                <Loader2 />
+              ) : (
+                <div className="flex-1">
+                  {allStories.length > 0 ? (
+                    <div className="grid grid-cols-3 gap-4 w-[90vw]">
+                      {allStories.map((item) => {
+                        return (
+                          <TravelStoryCard
+                            key={item._id}
+                            imgUrl={item.imageUrl}
+                            title={item.title}
+                            story={item.story}
+                            date={item.visitedDate}
+                            visitedLocation={item.visitedLocation}
+                            isFavourite={item.isFavourite}
+                            onEdit={() => handleEdit(item)}
+                            onClick={() => handleViewStory(item)}
+                            onFavouriteClick={() => updateIsFavorite(item)}
+                          />
+                        );
+                      })}
+                    </div>
+                  ) : (
+                    <div className="text-center py-4">
+                      No travel stories found.
+                    </div>
+                  )}
+                </div>
+              )}
 
-  <div className='w-[320px]'></div>
-      </div>
-      </div>
+              <div className="w-[320px]"></div>
+            </div>
+          </div>
 
-      <Modal 
-  isOpen={openAddEditModal.isShown}
-  onRequestClose={() => setOpenAddEditModal({ isShown: false, type: 'add', data: null })}
-  style={{ overlay: { backgroundColor: "rgba(0,0,0,0.2)", zIndex: 999 } }}
-  className="modal-box custom-scrollbar2"
->
-  <AddEditTravelStory 
-    type={openAddEditModal.type} 
-    storyInfo={openAddEditModal.data} 
-    onClose={() => setOpenAddEditModal({ isShown: false, type: 'add', data: null })}
-    getAllTravelStories={getAlltravelStories} 
-  />
-</Modal>
+          <Modal
+            isOpen={openAddEditModal.isShown}
+            onRequestClose={() =>
+              setOpenAddEditModal({ isShown: false, type: "add", data: null })
+            }
+            style={{
+              overlay: { backgroundColor: "rgba(0,0,0,0.2)", zIndex: 999 },
+            }}
+            className="modal-box custom-scrollbar2"
+          >
+            <AddEditTravelStory
+              type={openAddEditModal.type}
+              storyInfo={openAddEditModal.data}
+              onClose={() =>
+                setOpenAddEditModal({ isShown: false, type: "add", data: null })
+              }
+              getAllTravelStories={getAlltravelStories}
+            />
+          </Modal>
 
-    <button className='w-16 h-16 flex items-center justify-center rounded-full bg-[#05B6D3] hover:bg-cyan-400 fixed right-10 bottom-10' onClick={()=>(
-      setOpenAddEditModal({isShown: true, type:"add", data: null})
-    )}>
-      <MdAdd className='text-[32px] text-white' />
-    </button>
-      <ToastContainer />
-      </>
-    )}
+          <button
+            className="w-16 h-16 flex items-center justify-center rounded-full bg-[#05B6D3] hover:bg-cyan-400 fixed right-10 bottom-10"
+            onClick={() =>
+              setOpenAddEditModal({ isShown: true, type: "add", data: null })
+            }
+          >
+            <MdAdd className="text-[32px] text-white" />
+          </button>
+          <ToastContainer />
+        </>
+      )}
     </>
   );
 };
