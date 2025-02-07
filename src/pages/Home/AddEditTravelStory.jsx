df6afabea94df55bc6b015cc450f86bfdb0dea9d
incrediblesaizan1 <glorioussaizan1@gmail.com>
2025-02-07 12:40:54 +0530

    changes

--- src/pages/Home/AddEditTravelStory.jsx
+++ src/pages/Home/AddEditTravelStory.jsx
@@ -8,6 +8,8 @@ import { axiosInstance } from "../../utils/axiosInstance"
 import moment from "moment";
 import { toast } from "react-toastify";
 import uploadImage from "../../utils/uploadImage"
+import Loader2 from "../../components/common/Loader2";
+
 
 const AddEditTravelStory = ({
   storyInfo,
@@ -21,6 +23,7 @@ const AddEditTravelStory = ({
   const [story, setStory] = useState("");
   const [visitedLocation, setVisitedLocation] = useState([]);
   const [error, setError] = useState("")
+  const [isLoading, setIsLoading] = useState(false);
 
 
   const updateTravelStory = () =>{}
@@ -28,6 +31,7 @@ const AddEditTravelStory = ({
 
   const addNewTravelStory = async() =>{
     try {
+      setIsLoading(true)
       let imageUrl = ""
 
       if(storyImg){
@@ -50,14 +54,13 @@ const AddEditTravelStory = ({
         getAllTravelStories()
         onClose()
       }
-
+      setIsLoading(false)
     } catch (error) {
-      
+      setIsLoading(false)
     }
   }
  
   const handleAddOrUpdateClick = () => {
-    console.log({title, storyImg, story, visitedDate, visitedLocation})
 
     if(!title){
       setError("Please enter the title")
@@ -71,6 +74,8 @@ const AddEditTravelStory = ({
 
     setError("")
 
+
+
     if(type === "edit"){
       updateTravelStory();
     }else{
@@ -82,75 +87,79 @@ const AddEditTravelStory = ({
   const handleDeleteStoryImg = async () => {};
 
   return (
-    <div>
-      <div className="flex items-center justify-between">
-        <h5 className="text-xl font-medium text-slate-700">
-          {type === "add" ? "Add Story" : "Update Story"}
-        </h5>
-        <div>
-          <div className="flex items-center gap-3 bg-cyan-50/50 p-2 rounded-l-lg">
-            {type === "add" ? (
-              <button className="btn-small" onClick={handleAddOrUpdateClick}>
-                <MdAdd className="text-lg" /> ADD STORY
-              </button>
-            ) : (
-              <>
-                <button className="btn-small" onClick={handleAddOrUpdateClick}>
-                  <MdUpdate className="text-lg" /> UPDATE STORY
-                </button>
-              </>
-            )}
-            <button className="cursor-pointer" onClick={onClose}>
-              <MdClose className="text-xl text-slate-400" />
-            </button>
-          </div>
+    <>
+   {isLoading? <Loader2 /> :(
+     <div>
+     <div className="flex items-center justify-between">
+       <h5 className="text-xl font-medium text-slate-700">
+         {type === "add" ? "Add Story" : "Update Story"}
+       </h5>
+       <div>
+         <div className="flex items-center gap-3 bg-cyan-50/50 p-2 rounded-l-lg">
+           {type === "add" ? (
+             <button className="btn-small" onClick={handleAddOrUpdateClick}>
+               <MdAdd className="text-lg" /> ADD STORY
+             </button>
+           ) : (
+             <>
+               <button className="btn-small" onClick={handleAddOrUpdateClick}>
+                 <MdUpdate className="text-lg" /> UPDATE STORY
+               </button>
+             </>
+           )}
+           <button className="cursor-pointer" onClick={onClose}>
+             <MdClose className="text-xl text-slate-400" />
+           </button>
+         </div>
 
 {error && (
-  <p className="text-red-500 text-xs pt-2 text-right">{error}</p>
+ <p className="text-red-500 text-xs pt-2 text-right">{error}</p>
 )}
 
-        </div>
-      </div>
+       </div>
+     </div>
 
-      <div>
-        <div className=" flex-1 flex flex-col gap-2 pt-4">
-          <label className="input-label">TITLE</label>
-          <input
-            type="text"
-            className="text-2xl text-slate-950 outline-none"
-            placeholder="A day at a great wall"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-          <div className="my-3">
-            <DateSelector date={visitedDate} setDate={setVisitedDate} />
-          </div>
+     <div>
+       <div className=" flex-1 flex flex-col gap-2 pt-4">
+         <label className="input-label">TITLE</label>
+         <input
+           type="text"
+           className="text-2xl text-slate-950 outline-none"
+           placeholder="A day at a great wall"
+           value={title}
+           onChange={(e) => setTitle(e.target.value)}
+         />
+         <div className="my-3">
+           <DateSelector date={visitedDate} setDate={setVisitedDate} />
+         </div>
 
-          <ImageSelector
-            image={storyImg}
-            setImage={setStoryImg}
-            handleDeleteImg={handleDeleteStoryImg}
-          />
+         <ImageSelector
+           image={storyImg}
+           setImage={setStoryImg}
+           handleDeleteImg={handleDeleteStoryImg}
+         />
 
-          <div className="flex flex-col gap-2 mt-4">
-            <label className="input-label">STORY</label>
-            <textarea
-              type="text"
-              className="text-sm resize-none text-slate-950 outline-none bg-slate-100 p-2 roudned"
-              placeholder="Your Story"
-              rows={10}
-              value={story}
-              onChange={(e) => setStory(e.target.value)}
-            ></textarea>
-          </div>
+         <div className="flex flex-col gap-2 mt-4">
+           <label className="input-label">STORY</label>
+           <textarea
+             type="text"
+             className="text-sm resize-none text-slate-950 outline-none bg-slate-100 p-2 roudned"
+             placeholder="Your Story"
+             rows={10}
+             value={story}
+             onChange={(e) => setStory(e.target.value)}
+           ></textarea>
+         </div>
 
-          <div className="pt-3">
-            <label className="input-label">VISITED LOCATION</label>
-            <TagInput tags={visitedLocation} setTags={setVisitedLocation} />
-          </div>
-        </div>
-      </div>
-    </div>
+         <div className="pt-3">
+           <label className="input-label">VISITED LOCATION</label>
+           <TagInput tags={visitedLocation} setTags={setVisitedLocation} />
+         </div>
+       </div>
+     </div>
+   </div>
+   ) }
+    </>
   );
 };
 
